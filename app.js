12bf9b1c480e8ff71798ba3448383cc67ea0609a
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2023-02-15 16:29:17 -0300

    api brl ok

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ function getReal() {
     fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 .then((response) => response.json())
 
-.then((real) => {
-
+.then(function(real) {
+    /* Bitcoin*/
     const nameBtc = real.BTCBRL.code
-    console.log(nameBtc)
+    document.getElementById('name').innerHTML = nameBtc
 
-    const bitcoin = real.BTCBRL.bid
-    console.log(bitcoin)
+    const priceBtc = real.BTCBRL.bid
+    document.getElementById('price').innerHTML = parseFloat(priceBtc).toLocaleString('en', {style: 'currency', currency:'BRL'})
 
-    const change = real.BTCBRL.pctChange
-    console.log(change)
+    const pct = real.BTCBRL.pctChange
+    document.getElementById('change').innerHTML = parseFloat(pct) + '%'
     
+    /* DOLAR*/
+    const nameUs = real.USDBRL.code
+    document.getElementById('name2').innerHTML = nameUs
 
+    const priceUsd = real.USDBRL.bid
+    document.getElementById('price2').innerHTML = parseFloat(priceUsd).toLocaleString('en', {style: 'currency', currency: 'BRL'})
+
+    const pct2 = real.USDBRL.pctChange
+    document.getElementById('change2').innerHTML = parseFloat(pct2) + '%'
+
+    /* EURO*/
+    const nameEur = real.EURBRL.code
+    document.getElementById('name3').innerHTML = nameEur
+
+    const priceEur = real.EURBRL.bid
+    document.getElementById('price3').innerHTML = parseFloat(priceEur).toLocaleString('en',{style: 'currency', currency: 'BRL'})
+
+    const pct3 = real.EURBRL.pctChange
+    document.getElementById('change3').innerHTML = parseFloat(pct3) + '%'
 
 
    /* const dolar = real.USDBRL.bid
